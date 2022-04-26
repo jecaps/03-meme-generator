45ee691d6755a32fd1cf2ef929097fb9986d8006
Jerika Kanz <jerikacapila@gmail.com>
2022-04-26 16:02:28 +0200

    Complex state: Arrays

---
+++ 20-complex-state/src/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+
+function App() {
+  /**
+   * Challenge: Convert the code below to use an array
+   * held in state instead of a local variable. Initialize
+   * the state array with the same 2 items below
+   *
+   * Don't worry about fixing `addItem` quite yet.
+   */
+  const [thingsArray, setThingsArray] = React.useState(["Thing 1", "Thing 2"]);
+
+  function addItem() {
+    // We'll work on this next
+    setThingsArray((prevThingsArray) => {
+      return [...prevThingsArray, `Thing ${prevThingsArray.length + 1}`];
+    });
+
+    /*
+
+    my solution
+
+    setThingsArray(
+            prevThingsArray => prevThingsArray.concat(`Thing ${prevThingsArray.length + 1}`)
+        )
+    */
+  }
+
+  const thingsElements = thingsArray.map((thing) => <p key={thing}>{thing}</p>);
+
+  return (
+    <div>
+      <button onClick={addItem}>Add Item</button>
+      {thingsElements}
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
