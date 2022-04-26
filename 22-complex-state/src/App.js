95cf75b41063c558f9de83c1e7a2fa03abbf9c11
Jerika Kanz <jerikacapila@gmail.com>
2022-04-26 20:41:32 +0200

    Complex state:  Updating state objects

---
+++ 22-complex-state/src/App.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+export default function App() {
+  const [contact, setContact] = useState({
+    firstName: "John",
+    lastName: "Doe",
+    phone: "+1 (719) 555-1212",
+    email: "itsmyrealname@example.com",
+    isFavorite: false,
+  });
+
+  let starIcon = contact.isFavorite ? "star-filled.png" : "star-empty.png";
+
+  function toggleFavorite() {
+    setContact((prevContact) => {
+      return {
+        ...prevContact,
+        isFavorite: !prevContact.isFavorite,
+      };
+    });
+  }
+
+  return (
+    <main>
+      <article className="card">
+        <img
+          src={require("./images/user.png")}
+          className="card--image"
+          alt=""
+        />
+
+        <div>
+          <img
+            src={require(`./images/${starIcon}`)}
+            className="card--favorite"
+            onClick={toggleFavorite}
+            alt=""
+          />
+          <h2 className="card--name">
+            {contact.firstName} {contact.lastName}
+          </h2>
+          <p className="card--contact">{contact.phone}</p>
+          <p className="card--contact">{contact.email}</p>
+        </div>
+      </article>
+    </main>
+  );
+}
