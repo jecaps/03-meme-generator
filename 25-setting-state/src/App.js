d5c7f1eeedd544e84890288cca544ec28e23b061
Jerika Kanz <jerikacapila@gmail.com>
2022-04-27 18:45:41 +0200

    Setting State from Child Components

---
+++ 25-setting-state/src/App.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import Star from "./components/Star";
+
+export default function App() {
+  const [contact, setContact] = useState({
+    firstName: "John",
+    lastName: "Doe",
+    phone: "+1 (719) 555-1212",
+    email: "itsmyrealname@example.com",
+    isFavorite: false,
+  });
+
+  /**
+   * Challenge: Move the star image into its own component
+   * - It should receive a prop called `isFilled` that it
+   *   uses to determine which icon it will display
+   * - Import and render that component, passing the value of
+   *   `isFavorite` to the new `isFilled` prop.
+   * - Don't worry about the abiliity to flip this value quite yet.
+   *   Instead, you can test if it's working by manually changing
+   *   `isFavorite` in state above.
+   */
+
+  function toggleFavorite() {
+    setContact((prevContact) => ({
+      ...prevContact,
+      isFavorite: !prevContact.isFavorite,
+    }));
+  }
+
+  return (
+    <main>
+      <article className="card">
+        <img
+          src={require("./images/user.png")}
+          className="card--image"
+          alt=""
+        />
+        <div className="card--info">
+          <Star isFilled={contact.isFavorite} toggleHandler={toggleFavorite} />
+          <h2 className="card--name">
+            {contact.firstName} {contact.lastName}
+          </h2>
+          <p className="card--contact">{contact.phone}</p>
+          <p className="card--contact">{contact.email}</p>
+        </div>
+      </article>
+    </main>
+  );
+}
