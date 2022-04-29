c971f7fe99513096772a8870c44dc5571539ed8a
Jerika Kanz <jerikacapila@gmail.com>
2022-04-29 20:56:56 +0200

    Forms state object

---
+++ 42-forms-state-object/src/Form.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function Form() {
+  const [formData, setFormData] = useState({ firstName: "", lastName: "" });
+
+  console.log(formData);
+
+  function handleChange(event) {
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
+
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="First Name"
+        onChange={handleChange}
+        name="firstName"
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        onChange={handleChange}
+        name="lastName"
+      />
+    </form>
+  );
+}
